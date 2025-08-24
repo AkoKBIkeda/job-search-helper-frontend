7eb86621decf01e72e46bb3ba855274055a64087
AkoKBIkeda <akikoi1024@gmail.com>
2025-08-24 17:17:36 +1200

    Listing Company done

---
+++ src/components/CompanyList.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+// Set type for TS
+type Company = {
+  id: number;
+  name: string;
+  website: string;
+  jobTitle: string;
+};
+type Props = {
+  companies: Company[];
+};
+
+export const CompanyList = ({ companies }: Props) => {
+  const navigate = useNavigate();
+
+  // By clicking each company (row), navigate user to edit company information
+  const handleClick = (id: number) => {
+    navigate("/dashboard/companies/" + id + "/");
+  };
+  return (
+    <div>
+      <h2 className="text-primary text-2xl font-bold">Companies List</h2>
+      <div className="max-w-full border border-primary m-10">
+        <table className="min-w-full divide-y divide-primary text-left">
+          <thead className="bg-[#E5DC9F] text-primary text-center">
+            <tr className="border-r border-dotted px-4 py-2">
+              <th className="border-r border-dotted border-gray-800 px-4 py-2">
+                Company Name
+              </th>
+              <th className="border-r border-dotted border-gray-800 px-4 py-2">
+                Website
+              </th>
+              <th className="border-r border-dotted border-gray-800 px-4 py-2">
+                Job Roles
+              </th>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-primary">
+            {companies.map((company) => (
+              <tr
+                key={company.id}
+                onClick={() => handleClick(company.id)}
+                className={"cursor-pointer hover:bg-[#EAE7D3] transaction"}
+              >
+                <td className="border-r border-dotted border-gray-800 px-4 py-2">
+                  {company.name}
+                </td>
+                <td className="border-r border-dotted border-gray-800 px-4 py-2">
+                  {company.website}
+                </td>
+                <td className="border-r border-dotted border-gray-800 px-4 py-2">
+                  {company.jobTitle}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,20 +1,80 @@
 import React, { useState, useEffect, use } from "react";
 import Header from "../components/Header";
 import { useAuth } from "../hooks/useAuth";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import CompanyForm from "../forms/AddCompanyForm";
+import AddCompanyForm from "../forms/AddCompanyForm";
+import Button from "../components/Buttons";
+import { CompanyList } from "../components/CompanyList";
+import { BASE_URL } from "../constants";
+
+// Set type for TS
+type Company = {
+  id: number;
+  name: string;
+  website: string;
+  jobTitle: string;
+};
+
+// Set type for BE model names to avoid TS error
+type BECompany = {
+  id: number;
+  name: string;
+  website: string;
+  job_title: string;
+};
 
 const DashboardPage = () => {
   const { user, isAuthenticated, ready } = useAuth();
   const navigate = useNavigate();
+  const [companies, setCompanies] = useState<Company[]>([]);
+  const [loading, setLoading] = useState(true);
 
-  // Only authenticated users can access this page
+  // Receive BE model names to map them to FE names to avoid TS error
+  const frontendCompany = (be: BECompany): Company => ({
+    id: be.id,
+    name: be.name,
+    website: be.website || "",
+    jobTitle: be.job_title || "",
+  });
+
+  // Only authenticated users can see this page.
+  // Direct access to be redirected to the HomePage
   useEffect(() => {
     if (ready && !isAuthenticated) {
       navigate("/");
     }
   }, [isAuthenticated, navigate, ready]);
 
+  // Set Company for the current user
+  useEffect(() => {
+    const fetchCompanies = async () => {
+      setLoading(true);
+      const token = localStorage.getItem("token");
+      try {
+        const response = await fetch(BASE_URL + "/companies/", {
+          headers: {
+            Authorization: "Token " + token,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data: BECompany[] = await response.json();
+        console.log("fetched data: ", data);
+        // Set companies without causing naming error by using frontendCompany
+        setCompanies(data.map(frontendCompany));
+      } catch (error) {
+        console.error("Error fetching companies:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCompanies();
+  }, []);
+
+  // Show loading message while fetching data
   if (!ready || (isAuthenticated && user === null)) {
     return <div className="text-center">Loading...</div>;
   }
@@ -26,6 +86,18 @@ const DashboardPage = () => {
       <p className="text-primary text-lg font-semibold">
         Hi, {user?.username}! View and manage your job research here.
       </p>
+      <div className="flex flex-col items-end lg:pr-28">
+        <Button onClick={() => navigate("/dashboard/add-company")}>
+          Add Company
+        </Button>
+      </div>
+      <div>
+        {loading ? (
+          <div className="text-center">Loading companies...</div>
+        ) : (
+          <CompanyList companies={companies} />
+        )}
+      </div>
     </div>
   );
 };
