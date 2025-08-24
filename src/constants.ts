fd90be2b88f1077417bbc3b4e6ce029d0864a58d
AkoKBIkeda <akikoi1024@gmail.com>
2025-08-25 00:43:08 +1200

    BE url updated to Render's

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:8000";
+// export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://job-search-helper-fndk.onrender.com";  // Render's Url added
